59bd2bb6e84806367ffe4f77624c21d3b76597de
MANOJANASWARA <anaswaraaws@gmail.com>
2024-09-10 21:57:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,5 +20,4 @@ variable "ssh_private_key" {
 }
 variable "ssh_public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8sAMf6gNWEHRWfBZIemM2GqUc46MeaH64LZAbTT9ad"
-}
+  default = "/var/lib/jenkins/.ssh/id_ed25519.pub"}
